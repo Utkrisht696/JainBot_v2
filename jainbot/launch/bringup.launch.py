2ca067a339383f50eeef61a291d5ee0968913a76
Utkrisht696 <utkrishtjain2000@gmail.com>
2026-01-24 17:57:56 +0530

    updated time params for real sense cam

--- jainbot/launch/bringup.launch.py
+++ jainbot/launch/bringup.launch.py
@@ -77,6 +77,8 @@ def generate_launch_description():
             'enable_sync': 'true',
             'depth_module.depth_profile': '640x480x15',
             'rgb_camera.color_profile': '640x480x15',
+            'depth_module.global_time_enabled': 'true',
+            'rgb_camera.global_time_enabled': 'true',
             'align_depth.enable': 'true',
             'pointcloud.enable': 'false',
             'enable_infra1': 'false',
